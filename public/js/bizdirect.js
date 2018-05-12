7c3a83c57f6d3ad689db4c4632ff96f935cdf46d
Michelle <ellehcimj@gmail.com>
2018-05-11 23:27:42 -0700

    adds successful filters

--- public/js/bizdirect.js
+++ public/js/bizdirect.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
 // live search for abc order
-
 $('.biz-link').on('click', function(e) {
 	// stops page from refreshing
 	e.preventDefault();
@@ -18,6 +17,110 @@ $('.cat-link').on('click', function(e) {
 	$(this).toggleClass('active');
 	// filter organizations by category
 });
+
+$('.ae').on('click', function() {
+
+	// if user clicks this, then all other things not with ae disappear
+
+	$('div').filter('.org-ae').toggleClass('showOrg');
+
+	// $('.org-fj').toggleClass('hideOrg');
+	// $('.org-ko').toggleClass('hideOrg');
+	// $('.org-pt').toggleClass('hideOrg');
+	// $('.org-uz').toggleClass('hideOrg');
+
+});
+
+$('.fj').on('click', function() {
+
+$('div').filter('.org-fj').toggleClass('showOrg');
+
+	// if user clicks this, then all other things not with ae disappear
+	// $('.org-ae').toggleClass('hideOrg');
+	// $('.org-ko').toggleClass('hideOrg');
+	// $('.org-pt').toggleClass('hideOrg');
+	// $('.org-uz').toggleClass('hideOrg');
+
+});
+
+$('.ko').on('click', function() {
+
+
+$('div').filter('.org-ko').toggleClass('showOrg');
+	// if user clicks this, then all other things not with ae disappear
+	// $('.org-ae').toggleClass('hideOrg');
+	// $('.org-fj').toggleClass('hideOrg');
+	// $('.org-pt').toggleClass('hideOrg');
+	// $('.org-uz').toggleClass('hideOrg');
+
+});
+
+$('.pt').on('click', function() {
+
+$('div').filter('.org-pt').toggleClass('showOrg');
+
+	// if user clicks this, then all other things not with ae disappear
+	// $('.org-ae').toggleClass('hideOrg');
+	// $('.org-fj').toggleClass('hideOrg');
+	// $('.org-ko').toggleClass('hideOrg');
+	// $('.org-uz').toggleClass('hideOrg');
+
+});
+
+$('.uz').on('click', function() {
+
+$('div').filter('.org-uz').toggleClass('showOrg');
+
+	// if user clicks this, then all other things not with ae disappear
+	// $('.org-ae').toggleClass('hideOrg');
+	// $('.org-fj').toggleClass('hideOrg');
+
+});
+
+$('.food-drink').on('click', function() {
+
+	// if user clicks this, then all other things not with ae disappear
+
+	$('div').filter('.fd-box').toggleClass('showOrgFD');
+	// $('.ae-box').toggleClass('hideOrg');
+
+});
+
+$('.arts-ent').on('click', function() {
+
+	$('div').filter('.ae-box').toggleClass('showOrgAE');
+
+	
+
+	// if user clicks this, then all other things not with ae disappear
+	// $('.fd-box').toggleClass('hideOrg');
+
+});
+
+
+
+// var options = {
+//   valueNames: [ 'name', 'born' ]
+// };
+
+// var userList = new List('users', options);
+
+// $('.ae').on('click', function() {
+
+// });
+
+// $('.ae').on('click',function(){
+//   var $text = $(this).text();
+//   if($(this).hasClass( 'selected' )){
+//     userList.filter();
+//     $(this).removeClass('selected');
+//   } else {
+//     userList.filter(function(item) {
+//       return (item.values().born == $text);
+//     });
+//     $(this).addClass('selected');
+//   }
+// });
 
 // on hover have info appear
 
