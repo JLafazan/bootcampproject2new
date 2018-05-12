280024d3e51bff87e0cc9237c273e91781cff598
Michelle <ellehcimj@gmail.com>
2018-05-12 15:30:12 -0700

    adds some details to handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -17,6 +17,10 @@ module.exports = function (app) {
     res.render("index", { title: "StoryQuest" });
   });
 
+  app.get("/index", function (req, res) {
+    res.render("index", { title: "StoryQuest" });
+  });
+
   app.get("/cms", function (req, res) {
     res.render("cms", { title: "CMS - Express" });
   });
@@ -34,7 +38,25 @@ module.exports = function (app) {
   // res.sendFile(path.join(__dirname, "../views/favorites.html"));
   app.get('/favorites', function (req, res) {
     res.render('favorites', { title: "Users Favorites", favoritesStuff: ['basketball', "movies", "eating"], imageUrl: "/music.jpg" });
-  })
+  });
+
+  // gets biz org to display
+  app.get('/bizdirect', function (req, res) {
+    res.render('bizdirect', { 
+      title: "All Organizations",
+      // categories: ['Food & Drink', 'Arts & Entertainment'],
+      // categoriesClass: ['food-drink', 'arts-ent']
+      // imageUrl: "/music.jpg" 
+    });
+  });
+
+  // gets organizations page for each
+  app.get('/org1', function (req, res) {
+    res.render('org1', { 
+      orgName: "Buffet Club"
+      
+    });
+  });
 
   //})
 
