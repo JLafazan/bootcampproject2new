7f54938d3cea064299550c5f06392877298cc9df
Shaughn Roque <shaughnroque@Shaughns-MBP.localdomain>
2018-05-12 12:54:33 -0700

    models

--- models/post.js
+++ models/post.js
@@ -1,30 +1,39 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     var Post = sequelize.define("Post", {
-            title: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                validate: {
-                    len: [1]
-                }
-            },
-            body: {
-                type: DataTypes.TEXT,
-                allowNull: false,
-                validate: {
-                    len: [1]
-                }
-            },
-            category: {
-                type: DataTypes.STRING,
-                defaultValue: "Personal"
-            },
-            photo: {
-            type: DataTypes.BLOB,
-            allowNull: true,
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
             validate: {
                 len: [1]
             }
-          }
+        },
+        body: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        category: {
+            type: DataTypes.STRING,
+            defaultValue: "Event"
+        },
+        photo: {
+            type: DataTypes.BLOB,
+            allowNull: true
+            // validate: {
+            //     len: [1]
+            // }
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        updated_at: DataTypes.DATE,
+        deleted_at: DataTypes.DATE
+    }, {
+            underscored: true
+
         });
     return Post;
 };
