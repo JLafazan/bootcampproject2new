6ecd1dbb4d7d9847094c3f45c2e40cdfaefb1b6e
Michelle <ellehcimj@gmail.com>
2018-05-15 15:11:41 -0700

    mysteriously makes this work

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,29 +6,24 @@
 // =============================================================
 
 // Requiring our Todo model
-var express = require('express')
-var bodyParser = require('body-parser')
-var formidable = require('formidable')
+var express = require('express');
+var bodyParser = require('body-parser');
+var formidable = require('formidable');
 var db = require("../models");
 var path = require('path');
 fs = require('fs');
 var fileupload = require("express-fileupload");
-<<<<<<< HEAD
-//var multer = require('multer');
 
 var app = express();
-=======
+
 var multer = require('multer');
 
 // requiring passport to authenticate email and password of user
 var passport = require("../config/passport");
->>>>>>> master
 
 // Routes
 // =============================================================
 module.exports = function (app) {
-
-<<<<<<< HEAD
     // GET route for getting all of the events
     app.get("/api/events/", function (req, res) {
         db.Events.findAll({})
@@ -39,21 +34,26 @@ module.exports = function (app) {
 
     // Get route for returning events of a specific category
     app.get("/api/events/category/:category", function (req, res) {
-        db.Events.findAll({
-=======
+        db.Events.findAll({})
+
     // Using the passport.authenticate middleware with our local strategy.
     // If the user has valid login credentials, send them to the members page.
     // Otherwise the user will be sent an error
-    app.post("/api/login", passport.authenticate("local"), function (req, res) {
-        // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
+
+    //  line 53 comment
+        // Since we re doing a POST with javascript, we can't actually redirect that post into a GET request
         // So we're sending the user back the route to the members page because the redirect will happen on the front end
         // They won't get this or even be able to access this page if they aren't authed
-        res.redirect("/favorites");
-    });
 
-    // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
-    // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
-    // otherwise send back an error
+    // lines 62 - 74
+        // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
+        // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
+        // otherwise send back an error
+            app.post("/api/login", passport.authenticate("local"), function(req, res) {
+        
+                res.redirect("/favorites");
+        });
+  
     app.post("/api/signup", function (req, res) {
         console.log(req.body);
         db.User.create({
@@ -86,64 +86,64 @@ module.exports = function (app) {
     // Get route for returning posts of a specific category
     app.get("/api/posts/category/:category", function (req, res) {
         db.Post.findAll({
->>>>>>> master
+// >>>>>>> master
             where: {
                 category: req.params.category
             }
         })
-<<<<<<< HEAD
+// <<<<<<< HEAD
             .then(function (dbEvents) {
                 res.json(dbEvents);
-=======
-            .then(function (dbPost) {
-                res.json(dbPost);
->>>>>>> master
+// =======
+//             .then(function (dbPost) {
+//                 res.json(dbPost);
+// >>>>>>> master
             });
     });
 
     // Get route for retrieving a single post
-<<<<<<< HEAD
+// <<<<<<< HEAD
     app.get("/api/events/:id", function (req, res) {
         db.Events.findOne({
-=======
-    app.get("/api/posts/:id", function (req, res) {
-        db.Post.findOne({
->>>>>>> master
+// =======
+//     app.get("/api/posts/:id", function (req, res) {
+//         db.Post.findOne({
+// >>>>>>> master
             where: {
                 id: req.params.id
             }
         })
-<<<<<<< HEAD
+// <<<<<<< HEAD
             .then(function (dbEvents) {
                 res.json(dbEvents);
-=======
-            .then(function (dbPost) {
-                res.json(dbPost);
->>>>>>> master
+// =======
+//             .then(function (dbPost) {
+//                 res.json(dbPost);
+// >>>>>>> master
             });
     });
 
     // POST route for saving a new post
-<<<<<<< HEAD
+// <<<<<<< HEAD
     app.post("/api/events", function (req, res) {
-=======
-    app.post("/api/posts", function (req, res) {
->>>>>>> master
+// =======
+//     app.post("/api/posts", function (req, res) {
+// >>>>>>> master
         console.log(req.body);
 
         // use fs to save req.body.photo in /images
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
         db.Events.create({
-=======
-        db.Post.create({
->>>>>>> master
+// =======
+//         db.Post.create({
+// >>>>>>> master
             title: req.body.title,
             body: req.body.body,
             category: req.body.category,
             photo: req.body.photo,
         })
-<<<<<<< HEAD
+// <<<<<<< HEAD
             .then(function (dbEvents) {
                 res.json(dbEvents);
             });
@@ -152,27 +152,27 @@ module.exports = function (app) {
     // DELETE route for deleting events
     app.delete("/api/events/:id", function (req, res) {
         db.Events.destroy({
-=======
-            .then(function (dbPost) {
-                res.json(dbPost);
-            });
-    });
+// =======
+//             .then(function (dbPost) {
+//                 res.json(dbPost);
+//             });
+//     });
 
-    // DELETE route for deleting posts
-    app.delete("/api/posts/:id", function (req, res) {
-        db.Post.destroy({
->>>>>>> master
+//     // DELETE route for deleting posts
+//     app.delete("/api/posts/:id", function (req, res) {
+//         db.Post.destroy({
+// >>>>>>> master
             where: {
                 id: req.params.id
             }
         })
-<<<<<<< HEAD
+// <<<<<<< HEAD
             .then(function (dbEvents) {
                 res.json(dbEvents);
-=======
-            .then(function (dbPost) {
-                res.json(dbPost);
->>>>>>> master
+// =======
+//             .then(function (dbPost) {
+//                 res.json(dbPost);
+// >>>>>>> master
             });
     });
 
@@ -261,15 +261,15 @@ module.exports = function (app) {
 
 
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
     // PUT route for updating events
     // app.put("/api/events", function(req, res) {
     //     db.Events.update(req.body, {where: { id: req.body.id }}).then(function(dbEvents) {res.json(dbEvents)});
-=======
+// =======
     // PUT route for updating posts
     // app.put("/api/posts", function(req, res) {
     //     db.Post.update(req.body, {where: { id: req.body.id }}).then(function(dbPost) {res.json(dbPost)});
->>>>>>> master
+// >>>>>>> master
 
 
     // app.post('/upload', function(req, res) {
@@ -287,7 +287,5 @@ module.exports = function (app) {
     //         res.send('File uploaded!');
     //     });
     // });
-}
-
-
-
+});
+};
